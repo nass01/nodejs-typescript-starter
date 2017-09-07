248f509acaba1fa22cb907e2b2e4be9462f21250
nass01 <31698312+nass01@users.noreply.github.com>
2017-09-07 13:37:45 +0200

    Solved conflict

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,15 +2,12 @@
 import express = require("express");
 const app = express();
 
-<<<<<<< HEAD
 /*  DO NOT REMOVE THIS COMMENT!!
 Shouldn't this be changed into 
 app.get("/", (req: any, res: any) => { 
 Or even something more specific.
 In any case do not use "implicit any"  
 */
-=======
->>>>>>> 6e3ba0068b6f9a6dda42ba7521998f15244fcf26
 app.get("/", (req, res) => {
   res.send("Hello World2!");
 });
