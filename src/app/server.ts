6e3ba0068b6f9a6dda42ba7521998f15244fcf26
Halt001 <Halt001@users.noreply.github.com>
2017-09-07 13:24:33 +0200

    Removed unnecessary comment

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,11 +2,6 @@
 import express = require("express");
 const app = express();
 
-/* Shouldn't this be changed into 
-app.get("/", (req: any, res: any) => { 
-Or even something more specific.
-In any case do not use "implicit any"  
-*/
 app.get("/", (req, res) => {
   res.send("Hello World2!");
 });
